4be31ab85a2cdbe9a64c087d3188368027d0c02c
Jay Modi <jay.modi105@gmail.com>
2021-05-29 21:14:00 +1000

    Fix the bug in logic

--- src/components/TicTacToe.vue
+++ src/components/TicTacToe.vue
@@ -11,35 +11,39 @@ export default {
   data() {
     return {
       ticTacToe: [
-        [undefined,undefined,undefined],
-        [undefined,undefined,undefined],
-        [undefined,undefined,undefined]
+        ['','',''],
+        ['','',''],
+        ['','','']
       ],
       result: 'Start The Game'
     }
   },
   created() {
-    let [rowTotal, columnTotal, diagonalLeftTotal, diagonalRightTotal] = Array(3).fill(null);
+    let [diagonalLeftTotal, diagonalRightTotal] = Array(2).fill('');
 
     for(let row = 0; row < this.ticTacToe.length; row++) {
+      let [rowTotal, columnTotal] = Array(2).fill('');
+
       for(let column = 0; column < this.ticTacToe.length; column++) {
         // Row
         rowTotal += this.ticTacToe[row][column]
 
         // Column
         columnTotal += this.ticTacToe[column][row]
-
-        // Diagonal Left
-        diagonalLeftTotal = this.ticTacToe[row][row]
-
-        // Diagonal Right
-        diagonalRightTotal += this.ticTacToe[row][this.ticTacToe.length - row - 1]
       }
 
-      if (rowTotal == 3 || columnTotal == 3 || diagonalLeftTotal == 3 || diagonalRightTotal == 3) {
+      // Diagonal Left
+      diagonalLeftTotal += this.ticTacToe[row][row]
+
+      // Diagonal Right
+      diagonalRightTotal += this.ticTacToe[row][this.ticTacToe.length - row - 1]
+
+      if (rowTotal == '111' || columnTotal == '111' || diagonalLeftTotal == '111' || diagonalRightTotal == '111') {
         this.result = 'Player 1 Winner'
-      } else if (rowTotal == 0 || columnTotal == 0 || diagonalLeftTotal == 0 || diagonalRightTotal == 0) {
+        break
+      } else if (rowTotal == '222' || columnTotal == '222' || diagonalLeftTotal == '222' || diagonalRightTotal == '222') {
         this.result = 'Player 2 Winner'
+        break
       }
     }
   }
